83b52bad94d2d5d77bdc5f2cb1866b5822ddd496
gengithub17 <gensuke.hamada@gmail.com>
2023-06-23 21:57:46 +0900

    some progress in server.c

--- src/server/server.c
+++ src/server/server.c
@@ -21,10 +21,73 @@ typedef struct sockaddr_in SOCKADDR_IN4_T;
 typedef struct sockaddr_in6 SOCKADDR_IN6_T;
 
 THREAD_RETURN WOLFSSL_THREAD server_test(void* args){
+    // SOCKET_T sockfd = WOLFSSL_SOCKET_INVALID;
+
+    wolfSSL_method_func method = NULL;
+    WOLFSSL_CTX*    ctx = NULL;
+    WOLFSSL*    ssl     = NULL;
+
     int     argc = ((func_args*)args)->argc;
-    // char**  argv = ((func_args*)args)->argv;
-    printf("%d\n",argc);
-    printf("this is server_test\n");
+    char**  argv = ((func_args*)args)->argv;
+
+    word16 port = wolfSSLPort;
+
+    // const char* ourCert = NULL;
+    // const char* ourKey = NULL;
+
+    int ch;
+    
+    static const struct mygetopt_long_config long_options[] = {
+        {"help", 0, 257},
+        {0, 0, 0}
+    };
+
+    // int     version = SERVER_INVALID_VERSION;
+    while((ch = mygetopt_long(argc, argv, "?:"
+            "ab:c:defgh:i;jk:l:mnop:q:rstu;v:wxyz"
+            "A:B:CDE:F:GH:IJKL:M:NO:PQRS:TUVW:XYZ:"
+            "01:23:4567:89"
+            "@#", long_options, 0)) != -1){
+        switch(ch){
+            case 'p':
+                port = (word16)atoi(myoptarg);
+                break;
+            default:
+                ;
+        }
+    }
+
+    method = wolfTLSv1_3_server_method_ex;
+    
+    myoptind = 0;
+
+    if(method != NULL){
+        ctx = wolfSSL_CTX_new(method(NULL));
+        if(ctx == NULL)
+            err_sys("unable to get ctx");
+    }
+    if(wolfSSL_CTX_use_PrivateKey_file(ctx, "server-key.pem", WOLFSSL_FILETYPE_PEM)
+                                    != WOLFSSL_SUCCESS){
+        wolfSSL_CTX_free(ctx); ctx = NULL;
+        err_sys("can't load server private key file");
+    }
+    if(wolfSSL_CTX_use_certificate_file(ctx, "server-cert.pem", WOLFSSL_FILETYPE_PEM)
+                                    != WOLFSSL_SUCCESS){
+        wolfSSL_CTX_free(ctx); ctx = NULL;
+        err_sys("can't load server certificate file");
+    }
+
+    ssl = wolfSSL_new(ctx);
+    if(ssl == NULL){
+        wolfSSL_CTX_free(ctx); ctx = NULL;
+        err_sys("unable to get SSL object");
+    }
+
+    if(1==0){
+        printf("%d\n",argc);
+        printf("%d\n",port);
+    }
+
     return 0;
 }
 
@@ -35,6 +98,7 @@ int main(int argc, char** argv){
     args.argc = argc;
     args.argv = argv;
 
+    wolfSSL_Init();
     printf("this is server.c\n");
     return 0;
 }
