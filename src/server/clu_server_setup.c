1e9f3530991214564fc289642eae4e61dcbb1902
gengithub17 <gensuke.hamada@gmail.com>
2023-06-29 19:13:53 +0900

    port option

--- src/server/clu_server_setup.c
+++ src/server/clu_server_setup.c
@@ -8,13 +8,90 @@
 
 #ifndef WOLFCLU_NO_FILESYSTEM
 
-int wolfCLU_Server(int argc, char** argv){
-    func_args args;
-    args.argc = argc;
-    args.argv = argv;
-    server_test(&args);
-    printf("this is clu_server_setup.c\n");
-    return 0;
+static const struct option server_options[] = {
+    {"-port",           required_argument, 0, WOLFCLU_PORT},
+    {"-key" ,           required_argument, 0, WOLFCLU_KEYFILE},
+    {"-cert",           required_argument, 0, WOLFCLU_CERTFILE},
+    {0,0,0,0}
+};
+
+static const char portFlag[]        = "-p";
+static const char keyFileFlag[]     = "-k";
+static const char certFileFlag[]    = "-c";
+
+#define MAX_SERVER_ARGS 4
+
+/* return WOLFSSL_SUCCESS on success */
+static int _addServerArg(const char** args, const char* in, int* idx)
+{
+    int ret = WOLFCLU_SUCCESS;
+
+    if (*idx >= MAX_SERVER_ARGS) {
+        wolfCLU_LogError("Too many server args for array");
+        ret = WOLFCLU_FATAL_ERROR;
+    }
+    else {
+        args[*idx] = in;
+        *idx = *idx + 1;
+    }
+    return ret;
 }
 
-#endif
+#endif /* !WOLFCLU_NO_FILESYSTEM */
+
+int wolfCLU_Server(int argc, char** argv)
+{
+    func_args args;
+    int ret = WOLFCLU_SUCCESS;
+    int longIndex = 1;
+    int option;
+    
+    int serverArgc = 0;
+    const char* serverArgv[MAX_SERVER_ARGS];
+
+    ret = _addServerArg(serverArgv, "wolfclu", &serverArgc);
+
+    opterr = 0;
+    optind = 0;
+
+    while ((option = wolfCLU_GetOpt(argc, argv, "", server_options, &longIndex))
+                    != -1) {
+        switch (option) {
+            case WOLFCLU_PORT:
+                if (ret == WOLFCLU_SUCCESS) {
+                    ret = _addServerArg(serverArgv, portFlag, &serverArgc);
+                    if (ret == WOLFCLU_SUCCESS) {
+                        ret = _addServerArg(serverArgv, optarg, &serverArgc);
+                    }
+                }
+                break;
+            case WOLFCLU_KEYFILE:
+                if (ret == WOLFCLU_SUCCESS) {
+                    ret = _addServerArg(serverArgv, keyFileFlag, &serverArgc);
+                    if (ret == WOLFCLU_SUCCESS) {
+                        ret = _addServerArg(serverArgv, optarg, &serverArgc);
+                    }
+                }
+                break;
+            case WOLFCLU_CERTFILE:
+                if (ret == WOLFCLU_SUCCESS) {
+                    ret = _addServerArg(serverArgv, certFileFlag, &serverArgc);
+                    if (ret == WOLFCLU_SUCCESS) {
+                        ret = _addServerArg(serverArgv, optarg, &serverArgc);
+                    }
+                }
+                break;
+        }
+    }
+    for (int i = 0 ; i<serverArgc; i++){
+        printf("%d\t%s\n", i,serverArgv[i]);
+    }
+    if (ret == WOLFCLU_SUCCESS) {
+        args.argv = (char**)serverArgv;
+        args.argc = serverArgc;
+        server_test(&args);
+    }
+    
+    return ret;
+}
+
--- src/server/server.c
+++ src/server/server.c
@@ -14,6 +14,7 @@
 
 #include <wolfssl/test.h>
 #include <wolfssl/error-ssl.h>
+
 #include <wolfclu/clu_log.h>
 #include <wolfclu/server.h>
 
@@ -22,7 +23,9 @@ typedef struct sockaddr_in6 SOCKADDR_IN6_T;
 
 #define SV_MSG_SZ 32
 
-THREAD_RETURN WOLFSSL_THREAD server_test(void* args){
+
+THREAD_RETURN WOLFSSL_THREAD server_test(void* args)
+{
     SOCKET_T sockfd = WOLFSSL_SOCKET_INVALID;
     SOCKET_T clientfd = WOLFSSL_SOCKET_INVALID;
     SOCKADDR_IN_T clientAddr;
@@ -36,20 +39,18 @@ THREAD_RETURN WOLFSSL_THREAD server_test(void* args){
     char**  argv = ((func_args*)args)->argv;
 
     word16 port = wolfSSLPort;
+    const char* ourKey = NULL;
+    const char* ourCert = NULL;
     int useAnyAddr = 0;
     int dtlsUDP = 0;
     int    dtlsSCTP = 0;
     int    doListen = 1;
-
+    
     int ret;
 
     char msg[SV_MSG_SZ];
     int msgSz = 0;
     int finish = 0;
-    
-
-    // const char* ourCert = NULL;
-    // const char* ourKey = NULL;
 
     int ch;
     
@@ -58,15 +59,23 @@ THREAD_RETURN WOLFSSL_THREAD server_test(void* args){
         {0, 0, 0}
     };
 
+    
+
     // int     version = SERVER_INVALID_VERSION;
-    while((ch = mygetopt_long(argc, argv, "?:"
+    while ((ch = mygetopt_long(argc, argv, "?:"
             "ab:c:defgh:i;jk:l:mnop:q:rstu;v:wxyz"
             "A:B:CDE:F:GH:IJKL:M:NO:PQRS:TUVW:XYZ:"
             "01:23:4567:89"
-            "@#", long_options, 0)) != -1){
+            "@#", long_options, 0)) != -1) {
         switch(ch){
-            case 'p':
+            case 'p': /* port */
                 port = (word16)atoi(myoptarg);
+                break;
+            case 'k': /* key file */
+                ourKey = myoptarg;
+                break;
+            case 'c': /* cert file */
+                ourCert = myoptarg;
                 break;
             default:
                 ;
@@ -77,58 +86,56 @@ THREAD_RETURN WOLFSSL_THREAD server_test(void* args){
     
     myoptind = 0;
 
-    if(method != NULL){
+    if (method != NULL) {
         ctx = wolfSSL_CTX_new(method(NULL));
         if(ctx == NULL)
             err_sys("unable to get ctx");
     }
-    if(wolfSSL_CTX_use_PrivateKey_file(ctx, "server-key.pem", WOLFSSL_FILETYPE_PEM)
-                                    != WOLFSSL_SUCCESS){
+    if (wolfSSL_CTX_use_PrivateKey_file(ctx, ourKey, 
+                    WOLFSSL_FILETYPE_PEM) != WOLFSSL_SUCCESS) {
         wolfSSL_CTX_free(ctx); ctx = NULL;
         err_sys("can't load server private key file");
     }
-    if(wolfSSL_CTX_use_certificate_file(ctx, "server-cert.pem", WOLFSSL_FILETYPE_PEM)
-                                    != WOLFSSL_SUCCESS){
+    if (wolfSSL_CTX_use_certificate_file(ctx, ourCert, 
+                    WOLFSSL_FILETYPE_PEM) != WOLFSSL_SUCCESS) {
         wolfSSL_CTX_free(ctx); ctx = NULL;
         err_sys("can't load server certificate file");
     }
 
-    while(!finish){
+    while (!finish) {
         ssl = wolfSSL_new(ctx);
-        if(ssl == NULL){
+        if (ssl == NULL) {
             wolfSSL_CTX_free(ctx); ctx = NULL;
             err_sys("unable to get SSL object");
             break;
         }
-        // tcp_listen(&sockfd, &port, useAnyAddr, dtlsUDP, dtlsSCTP);
         tcp_accept(&sockfd, &clientfd, args, port, useAnyAddr, dtlsUDP, dtlsSCTP, 0, 
             doListen, &clientAddr, &clientLen);
-        // doListen = 0; /* Don't listen next time */
         
-        if((ret = SSL_set_fd(ssl, clientfd)) != WOLFSSL_SUCCESS){
+        if ((ret = SSL_set_fd(ssl, clientfd)) != WOLFSSL_SUCCESS) {
             err_sys("error in setting fd");
             finish = 1;
             goto sslclose;
         }
-        if((ret = SSL_accept(ssl)) != WOLFSSL_SUCCESS){
+        if ((ret = SSL_accept(ssl)) != WOLFSSL_SUCCESS) {
             err_sys("error in SSL accept");
             finish = 1;
             goto sslclose;
         }
         
         memset(msg, 0, SV_MSG_SZ);
-        if((msgSz = wolfSSL_read(ssl, msg, sizeof(msg)-1))==0){
+        if ((msgSz = wolfSSL_read(ssl, msg, sizeof(msg)-1))==0) {
             err_sys("error in SSL read");
             finish = 1;
             goto sslclose;
         }
         printf("Message : %s\n",msg);
-        if(wolfSSL_write(ssl, msg, msgSz) != msgSz){
+        if (wolfSSL_write(ssl, msg, msgSz) != msgSz) {
             err_sys("error in SSL_write");
             finish = 1;
             goto sslclose;
         }
-        if(strcmp(msg,"finish") == 0){
+        if (strcmp(msg,"finish") == 0) {
             finish = 1;
         }
 sslclose:
@@ -141,17 +148,13 @@ sslclose:
     wolfSSL_CTX_free(ctx);
     wolfSSL_Cleanup();
 
-    if(1==0){
-        printf("%d\n",argc);
-        printf("%d\n",port);
-    }
-
     return 0;
 }
 
 // #define NO_MAIN_DRIVER
 #ifndef NO_MAIN_DRIVER
-int main(int argc, char** argv){
+int main(int argc, char** argv)
+{
     func_args args;
     args.argc = argc;
     args.argv = argv;
