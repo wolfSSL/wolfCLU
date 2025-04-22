7b77a6ffc1046c46ba87aafaa75f9b5e617cfb93
Lealem Amedie <lealem@wolfssl.com>
2025-04-21 21:44:37 -0600

    Add -untrusted option to verify

--- tests/x509/x509-verify-test.sh
+++ tests/x509/x509-verify-test.sh
@@ -17,7 +17,7 @@ if [ $? == 0 ]; then
     echo "Failed on test \"./wolfssl verify ./certs/server-cert.pem\""
     exit 99
 fi
-echo "$RESULT" | grep "Err (-188): certificate verify failed"
+echo "$RESULT" | grep "Err (20): unable to get local issuer certificate"
 if [ $? != 0 ]; then
     echo "Unexpected error result on test \"./wolfssl verify ./certs/server-cert.pem\""
     exit 99
@@ -28,7 +28,7 @@ if [ $? == 0 ]; then
     echo "Failed on test \"./wolfssl verify ./certs/ca-cert.pem\""
     exit 99
 fi
-echo "$RESULT" | grep "Err (-275): ASN self-signed certificate error"
+echo "$RESULT" | grep "Err (18): self-signed certificate in certificate chain"
 if [ $? != 0 ]; then
     echo "Unexpected error result on test \"./wolfssl verify ./certs/ca-cert.pem\""
     exit 99
